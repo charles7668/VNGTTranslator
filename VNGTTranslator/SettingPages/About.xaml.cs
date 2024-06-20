47bc26700c268db2af37666bf77305d0c15352a2
charles <charles76680119@gmail.com>
2024-06-20 15:28:25 +0800

    [Feat] Add information to About page

--- VNGTTranslator/SettingPages/About.xaml.cs
+++ VNGTTranslator/SettingPages/About.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VNGTTranslator.SettingPages
 {
     /// <summary>
     /// About.xaml 的互動邏輯
     /// </summary>
-    public partial class About : Page
+    public partial class About : INotifyPropertyChanged
     {
         public About()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public string VersionText => $"Version: {Assembly.GetExecutingAssembly().GetName().Version}";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)
+            {
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
