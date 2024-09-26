fb5b414868520eb0bec708777d7f9754b35a95a1
charles7668 <charles76680119@gmail.com>
2024-09-26 09:16:09 +0800

    Refactor the window capture by using the `PrintWindow` function to support more applications.

--- Win32ApiLibrary/ScreenHelper.cs
+++ Win32ApiLibrary/ScreenHelper.cs
@@ -1,11 +1,13 @@
-﻿using Windows.Win32;
+﻿using System.Drawing.Imaging;
+using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.Storage.Xps;
 using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace Win32ApiLibrary
 {
-    public class ScreenHelper
+    public static class ScreenHelper
     {
         /// <summary>
         /// get screenshot base on window handle
@@ -14,32 +16,18 @@ namespace Win32ApiLibrary
         /// <returns></returns>
         public static Bitmap GetWindowCapture(IntPtr handle)
         {
-            // get te hDC of the target window
-            HDC hdcSrc = PInvoke.GetWindowDC((HWND)handle);
             PInvoke.ShowWindow((HWND)handle, SHOW_WINDOW_CMD.SW_SHOWNA);
             // get the size
             PInvoke.GetWindowRect((HWND)handle, out RECT windowRect);
-            int width = windowRect.right - windowRect.left;
-            int height = windowRect.bottom - windowRect.top;
-            // create a device context we can copy to
-            HDC hdcDest = PInvoke.CreateCompatibleDC(hdcSrc);
-            // create a bitmap we can copy it to,
-            // using GetDeviceCaps to get the width/height
-            HBITMAP hBitmap = PInvoke.CreateCompatibleBitmap(hdcSrc, width, height);
-            // select the bitmap object
-            HGDIOBJ hOld = PInvoke.SelectObject(hdcDest, hBitmap);
-            // bitblt over
-            PInvoke.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, ROP_CODE.SRCCOPY | ROP_CODE.CAPTUREBLT);
-            // restore selection
-            PInvoke.SelectObject(hdcDest, hOld);
-            // clean up
-            PInvoke.DeleteDC(hdcDest);
-            PInvoke.ReleaseDC((HWND)handle, hdcSrc);
-            // get a .NET image object for it
-            Bitmap img = Image.FromHbitmap(hBitmap);
-            // free up the Bitmap object
-            PInvoke.DeleteObject(hBitmap);
-            return img;
+            var bmp = new Bitmap(windowRect.Width, windowRect.Height, PixelFormat.Format32bppArgb);
+            var gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+            // For param3, use 2 for DirectX windows. Note that 2 is not defined in the Win32 API.
+            // Reference: https://stackoverflow.com/questions/891345/get-a-screenshot-of-a-specific-application
+            PInvoke.PrintWindow((HWND)handle, (HDC)hdcBitmap, (PRINT_WINDOW_FLAGS)2);
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+            return bmp;
         }
     }
 }
